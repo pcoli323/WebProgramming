fd0f7cd3c0afb455b48f7de2e3d7682f5603c4e3
pcoli323 <pcoli323@gmail.com>
2017-09-10 17:19:04 +0900

    로그인 인터셉터 기능 추가

---
+++ Tour/src/main/java/org/tour/interceptor/LoginInterceptor.java
@@ -0,0 +1,36 @@
+package org.tour.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.ui.ModelMap;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class LoginInterceptor extends HandlerInterceptorAdapter {
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		HttpSession session = request.getSession();
+		ModelMap modelMap = modelAndView.getModelMap();
+		Object userVO = modelMap.get("userVO");
+		if(userVO!=null) {
+			session.setAttribute("login", userVO);
+			//response.sendRedirect("/main");
+		}
+		super.postHandle(request, response, handler, modelAndView);
+	}
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		HttpSession session = request.getSession();
+		if(session.getAttribute("login")!=null) {
+			session.removeAttribute("login");
+		}
+		return super.preHandle(request, response, handler);
+	}
+
+}
