1c952a54949d56cfa7f727195132f13c90bdad1d
yeong <phy0110@icloud.com>
2017-09-21 02:18:39 +0900

    modifyView

--- Tour/src/main/java/org/tour/controller/CourseMakeController.java
+++ Tour/src/main/java/org/tour/controller/CourseMakeController.java
@@ -17,17 +17,13 @@ import javax.servlet.http.HttpSession;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.tour.domain.FollowVO;
 import org.tour.dto.AreaDTO;
 import org.tour.dto.SelectedAreaDTO;
 import org.tour.service.AreaService;
@@ -46,27 +42,22 @@ public class CourseMakeController {
 	@RequestMapping(value = "/course/make/add1", method = RequestMethod.GET)
 	public String add1(Locale locale, Model model) throws Exception {
 		
+		/*
+		// DB에서 지역, 시군구 데이터를 가져오는 부분
+		model.addAttribute("areaList", areaService.selectAll());
+		model.addAttribute("sigunguList", sigunguService.selectAll());
+		
+		return "/course/make/add1";
+		*/
 		Gson gson = new Gson();
 		model.addAttribute("areaList", gson.toJson(areaService.selectAll()));
 		model.addAttribute("sigunguList", gson.toJson(sigunguService.selectAll()));
 		
-		return "/course/make/add1";
-	}
-	
-	@RequestMapping(value = "/course/make/add1/save", method = RequestMethod.POST)
-	public void add1Save(HttpServletRequest request, @RequestBody String data) throws ParseException {
-		
-		JSONParser parser = new JSONParser();
-		Object obj = parser.parse( data );
-		JSONArray jsonarray = (JSONArray)obj;
-		
-		HttpSession session = request.getSession();
-		session.setAttribute("arealist", jsonarray);
-		
+		return "/course/make/newAdd1";
 	}
 	
 	@RequestMapping(value = "/course/make/add2", method = RequestMethod.GET)
-	public void add2(HttpServletRequest request, Model model) throws UnsupportedEncodingException, IOException {
+	public String add2(HttpServletRequest request, Model model) throws UnsupportedEncodingException, IOException {
 		
 		/*
 		String idList = request.getParameter("idList");
@@ -138,7 +129,30 @@ public class CourseMakeController {
 		}
 		*/
 		
+		String addr = "http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaBasedList";
+		String serviceKey = "?serviceKey=ZMWqGPxD2Y1ds3Sr4PJcz62ZsAzs3Wwu2%2FIWwyGFvbQXC0wCQQHcyaYY%2B6H8LDIVst1GREAN9DNoE2mUHU2%2Ffg%3D%3D";
+		String parameter = "";
+		String type = "&_type=json";
 		
+		parameter += "&MobileOS=ETC";
+		parameter += "&MobileApp=Tour";
+		parameter += "&arrange=B";
+		parameter += "&areaCode=1";
+		
+		addr += serviceKey + parameter + type;
+		
+		URL url = new URL(addr);
+		InputStreamReader isr = new InputStreamReader(url.openConnection().getInputStream(), "UTF-8");
+		JSONObject jsonObject = (JSONObject)JSONValue.parse(isr);
+		JSONObject dataObject = (JSONObject) jsonObject.get("response");
+		JSONObject dataObject2 = (JSONObject) dataObject.get("body");
+		JSONObject dataObject3 = (JSONObject) dataObject2.get("items");
+		JSONArray memberArray = (JSONArray) dataObject3.get("item");
+		
+		HttpSession session = request.getSession();
+		session.setAttribute("list", memberArray);
+		
+		return "/course/make/test";
 		
 		/*
 		String addr = "http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaBasedList";
@@ -234,47 +248,29 @@ public class CourseMakeController {
 		*/
 	}
 	
-	
-	@RequestMapping(value = "/course/make/test", method = RequestMethod.GET)
-	public void test(HttpServletRequest request, Locale locale, Model model) throws UnsupportedEncodingException, IOException {
-		
-		String addr = "http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaBasedList";
-		String serviceKey = "?serviceKey=ZMWqGPxD2Y1ds3Sr4PJcz62ZsAzs3Wwu2%2FIWwyGFvbQXC0wCQQHcyaYY%2B6H8LDIVst1GREAN9DNoE2mUHU2%2Ffg%3D%3D";
-		String parameter = "";
-		String type = "&_type=json";
-		
-		parameter += "&MobileOS=ETC";
-		parameter += "&MobileApp=Tour";
-		parameter += "&arrange=B";
-		parameter += "&areaCode=1";
-		
-		addr += serviceKey + parameter + type;
-		
-		URL url = new URL(addr);
-		InputStreamReader isr = new InputStreamReader(url.openConnection().getInputStream(), "UTF-8");
-		JSONObject jsonObject = (JSONObject)JSONValue.parse(isr);
-		JSONObject dataObject = (JSONObject) jsonObject.get("response");
-		JSONObject dataObject2 = (JSONObject) dataObject.get("body");
-		JSONObject dataObject3 = (JSONObject) dataObject2.get("items");
-		JSONArray memberArray = (JSONArray) dataObject3.get("item");
-		
+	/*
+	@RequestMapping(value = "/course/make/test2", method = RequestMethod.GET)
+	public void test2(HttpServletRequest request, Locale locale, Model model) {
 		HttpSession session = request.getSession();
-		session.setAttribute("list", memberArray);
-		
+		JSONArray jsonArr = (JSONArray) session.getAttribute("list");
+		for(int i=0; i<jsonArr.size(); i++) {
+			JSONObject data = (JSONObject)jsonArr.get(i);
+			System.out.println(data.get("title"));
+		}
 	}
+	*/
 	
 	@RequestMapping(value = "/course/make/modify", method = RequestMethod.GET)
 	public String modify(Locale locale, Model model) {
 		return "course/make/modify";
 	}
-	
 	@RequestMapping(value = "/course/make/modify/remove/{Status}", method = RequestMethod.POST)
 	public ResponseEntity<Integer> remove(HttpServletRequest request, @PathVariable("Status") int Status){
 		
 		ResponseEntity<Integer> entity = null;
 		try {
 			HttpSession session = request.getSession();
-			ArrayList<String> list = (ArrayList<String>)session.getAttribute("lists");
+			JSONArray list = (JSONArray)session.getAttribute("list");
 			
 			list.remove(Status);
 			
@@ -286,3 +282,4 @@ public class CourseMakeController {
 		return entity;
 	}
 }
+
