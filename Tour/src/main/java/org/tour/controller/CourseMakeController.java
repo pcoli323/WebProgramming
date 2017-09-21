843238254c9454e88880d60420484290d0c0122a
pcoli323 <pcoli323@gmail.com>
2017-09-21 18:12:52 +0900

    /course/make/add2 커밋

--- Tour/src/main/java/org/tour/controller/CourseMakeController.java
+++ Tour/src/main/java/org/tour/controller/CourseMakeController.java
@@ -62,7 +62,7 @@ public class CourseMakeController {
 		
 		HttpSession session = request.getSession();
 		session.setAttribute("idList", jsonarray);
-		System.out.println(jsonarray);
+		//System.out.println(jsonarray);
 	}
 	
 	@RequestMapping(value = "/course/make/add2", method = RequestMethod.GET)
