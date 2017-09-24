1ecadf61944754f5fadf0f6e8ad01d1f4bd5d42b
pcoli323 <pcoli323@gmail.com>
2017-09-24 16:28:16 +0900

    충돌해결

--- Tour/src/main/java/org/tour/persistence/CourseInfoDAO.java
+++ Tour/src/main/java/org/tour/persistence/CourseInfoDAO.java
@@ -19,10 +19,6 @@ public interface CourseInfoDAO {
 	public List<CourseInfoVO> readAll(int courseNumber) throws Exception;
 	
 	public void add(CourseInfoVO vo) throws Exception;
-<<<<<<< HEAD
 	
 	public void courseInfoAdd(CourseInfoVO vo) throws Exception;
-=======
-
->>>>>>> 08dbe60be2bca58bafc184f2fd23fbaac6b0c86d
 }
